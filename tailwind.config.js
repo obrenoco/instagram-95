68fbfc25ec0744bb1707acd9455f568904eb2f72
Breno Romeiro <breno.romeiro@Brenos-MacBook-Pro.local>
2024-02-27 13:37:34 -0300

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        gray: { DEFAULT: "#b8b7b7", dark: "#7f7f7f", light: "#C0C0C0" },
+        "blue-windows": "#017d7d",
+      },
+      borderWidth: {
+        1: "1px;",
+      },
+    },
+  },
+  plugins: [],
+};
